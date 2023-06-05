49fbb87e17e1a7e1fcd55e804f6092c22935806b
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-06-05 10:31:34 -0300

    conditional rendering of cards

--- src/components/body_components/project_cards/Card.js
+++ src/components/body_components/project_cards/Card.js
@@ -2,27 +2,31 @@ import React from "react";
 import { FaGithub, FaLink } from "react-icons/fa";
 import "../../../styles/Card.css";
 
-export function Card({ projectData }) {
+export function Card({ primaryCard, secondaryCard, projectData }) {
 
   let sourcePath1 = {projectData.defaultImage};
   let sourcePath2 = {projectData.alternateImage};
 
   const [isHovered, setIsHovered] = useState(false);
 
-  return (
-    <div className="card" style={{ width: "18rem" }}>
-      <img className="card-img-top" src= alt="Card image cap" />
-      <div className="card-body">
-        <h5 className="card-title">{projectData.title}</h5>
-        <p className="card-text">
-          {projectData.description}
-        </p>
-        <a href={projectData.gitHubURL} className="socialMediaIcon">
-          <FaGithub />
-        </a>
-        <a href={projectData.deployedURL}>
-          <FaLink />
-        </a>
+    return (
+    <div className={`row`}>
+      <div className={`col-${primaryCard === true ? '12 primaryCard' : secondaryCard === true ? '6 secondaryCard' : '4 tertiaryCard'}`}>
+        <div className="card" >
+          <img className="card-img-top" src={isHovered ? sourcePath2 : sourcePath1} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title">{projectData.title}</h5>
+            <p className="card-text">
+              {projectData.description}
+            </p>
+            <a href={projectData.gitHubURL} className="socialMediaIcon">
+              <FaGithub />
+            </a>
+            <a href={projectData.deployedURL}>
+              <FaLink />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
