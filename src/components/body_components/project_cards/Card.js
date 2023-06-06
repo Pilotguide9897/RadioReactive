b7d73ed5241777e7e2aacc5639b4aeceb9f5fd77
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-06-06 12:03:44 -0300

    cards on their way

--- src/components/body_components/project_cards/Card.js
+++ src/components/body_components/project_cards/Card.js
@@ -10,24 +10,42 @@ export function Card({ primaryCard, secondaryCard, projectData }) {
   const [isHovered, setIsHovered] = useState(false);
 
     return (
-    <div className={`row`}>
-      <div className={`col-${primaryCard === true ? '12 primaryCard' : secondaryCard === true ? '6 secondaryCard' : '4 tertiaryCard'}`}>
-        <div className="card" >
-          <img className="card-img-top" src={isHovered ? sourcePath2 : sourcePath1} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} alt="Project" />
-          <div className="card-body">
-            <h5 className="card-title">{projectData.title}</h5>
-            <p className="card-text">
-              {projectData.description}
-            </p>
-            <a href={projectData.gitHubURL} className="socialMediaIcon">
-              <FaGithub />
-            </a>
-            <a href={projectData.deploymentURL}>
-              <FaLink />
-            </a>
+      <div className={`row`}>
+        <div
+          className={`col-${
+            primaryCard === true
+              ? "12 primaryCard"
+              : secondaryCard === true
+              ? "6 secondaryCard"
+              : "4 tertiaryCard"
+          } allCards`}
+        >
+          <div>
+            <div className="card-body">
+              <div
+                className="card"
+                id="cardImageContainer"
+                style={{
+                  backgroundImage: `url(${
+                    isHovered ? sourcePath2 : sourcePath1
+                  })`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                }}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+              ></div>
+              <h2 className="card-title">{projectData.title}</h2>
+              <p className="card-text">{projectData.description}</p>
+              <a href={projectData.gitHubURL} className="socialMediaIcon">
+                <FaGithub />
+              </a>
+              <a href={projectData.deploymentURL} className="socialMediaIcon">
+                <FaLink />
+              </a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
 };
